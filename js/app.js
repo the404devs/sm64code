dc8683abcc7f4c24d42dd90ee5775ebfe671c67c
Owen Bowden <the404devs@gmail.com>
2024-08-05 21:12:18 -0400

    some more tweaks

--- js/app.js
+++ js/app.js
@@ -9,6 +9,141 @@ const descriptiveNames = {
 	"ique": "iQue (Chinese)"
 }
 
+const memoryMappings = {
+	"ntsc-u": {
+		"hat_shirt": {
+			"shadow": ["8107EC38", "8107EC3A"],
+			"main": ["8107EC40", "8107EC42"]
+		},
+		"hair": {
+			"shadow": ["8107EC98", "8107EC9A"],
+			"main": ["8107ECA0", "8107ECA2"]
+		},
+		"skin": {
+			"shadow": ["8107EC80", "8107EC82"],
+			"main": ["8107EC88", "8107EC8A"]
+		},
+		"gloves": {
+			"shadow": ["8107EC50", "8107EC52"],
+			"main": ["8107EC58", "8107EC5A"]
+		},
+		"overalls": {
+			"shadow": ["8107EC20", "8107EC22"],
+			"main": ["8107EC28", "8107EC2A"]
+		},
+		"shoes": {
+			"shadow": ["8107EC68", "8107EC6A"],
+			"main": ["8107EC70", "8107EC72"]
+		}
+	},
+	"ntsc-j": {
+		"hat_shirt": {
+			"shadow": ["8107BDD8", "8107BDDA"],
+			"main": ["8107BDE0", "8107BDE2"]
+		},
+		"hair": {
+			"shadow": ["8107BE38", "8107BE3A"],
+			"main": ["8107BE40", "8107BE42"]
+		},
+		"skin": {
+			"shadow": ["8107BE20", "8107BE22"],
+			"main": ["8107BE28", "8107BE2A"]
+		},
+		"gloves": {
+			"shadow": ["8107BDF0", "8107BDF2"],
+			"main": ["8107BDF8", "8107BDFA"]
+		},
+		"overalls": {
+			"shadow": ["8107BDC0", "8107BDC2"],
+			"main": ["8107BDC8", "8107BDCA"]
+		},
+		"shoes": {
+			"shadow": ["8107BE08", "8107BE0A"],
+			"main": ["8107BE10", "8107BE12"]
+		}
+	},
+	"pal": {
+		"hat_shirt": {
+			"shadow": ["810742F8", "810742FA"],
+			"main": ["81074300", "81074302"]
+		},
+		"hair": {
+			"shadow": ["81074358", "8107435A"],
+			"main": ["81074360", "81074362"]
+		},
+		"skin": {
+			"shadow": ["81074340", "81074342"],
+			"main": ["81074348", "8107434A"]
+		},
+		"gloves": {
+			"shadow": ["81074310", "81074312"],
+			"main": ["81074318", "8107431A"]
+		},
+		"overalls": {
+			"shadow": ["810742E0", "810742E2"],
+			"main": ["810742E8", "810742EA"]
+		},
+		"shoes": {
+			"shadow": ["81074328", "8107432A"],
+			"main": ["81074330", "81074332"]
+		}
+	},
+	"shindou": {
+		"hat_shirt": {
+			"shadow": ["8107BDD8", "8107BDDA"],
+			"main": ["8107BDE0", "8107BDE2"]
+		},
+		"hair": {
+			"shadow": ["8107BE38", "8107BE3A"],
+			"main": ["8107BE40", "8107BE42"]
+		},
+		"skin": {
+			"shadow": ["8107BE20", "8107BE22"],
+			"main": ["8107BE28", "8107BE2A"]
+		},
+		"gloves": {
+			"shadow": ["8107BDF0", "8107BDF2"],
+			"main": ["8107BDF8", "8107BDFA"]
+		},
+		"overalls": {
+			"shadow": ["8107BDC0", "8107BDC2"],
+			"main": ["8107BDC8", "8107BDCA"]
+		},
+		"shoes": {
+			"shadow": ["8107BE08", "8107BE0A"],
+			"main": ["8107BE10", "8107BE12"]
+		}
+	},
+	"ique": {
+		"hat_shirt": {
+			"shadow": ["810610A8", "810610AA"],
+			"main": ["810610B0", "810610B2"]
+		},
+		"hair": {
+			"shadow": ["81061108", "8106110A"],
+			"main": ["81061110", "81061112"]
+		},
+		"skin": {
+			"shadow": ["810610F0", "810610F2"],
+			"main": ["810610F8", "810610FA"]
+		},
+		"gloves": {
+			"shadow": ["810610C0", "810610C2"],
+			"main": ["810610C8", "810610CA"]
+		},
+		"overalls": {
+			"shadow": ["81061090", "81061092"],
+			"main": ["81061098", "8106109A"]
+		},
+		"shoes": {
+			"shadow": ["810610D8", "810610DA"],
+			"main": ["810610E0", "810610E2"]
+		}
+	}
+}
+
+const guideText = "┐ Hat & Shirt\n│\n│\n┘\n\n┐ Hair\n│\n│\n┘\n\n┐ Face\n│\n│\n┘\n\n┐ Gloves\n│\n│\n┘\n\n┐ Overalls\n│\n│\n┘\n\n┐ Shoes\n│\n│\n┘";
+
 function switchGameVersion() {
 	gameVersion = document.getElementById("gameVer").value;
 	generate();
@@ -166,141 +301,9 @@ function generate(){
 	${memoryMappings[gameVersion].shoes.main[1]} ${shoe_base[2]}00
 	`;
 
-	document.getElementById("code").value = code.replaceAll("\t","").replace("\n", "").toUpperCase();
 
+	document.getElementById("code").value = code.replaceAll("\t","").replace("\n", "").toUpperCase();
+	document.getElementById("guide").value = guideText;
 	document.getElementById("type").innerHTML = `This code is for the <b>${descriptiveNames[gameVersion]}</b> version.`;
 
-}
-
-const memoryMappings = {
-	"ntsc-u": {
-		"hat_shirt": {
-			"shadow": ["8107EC38", "8107EC3A"],
-			"main": ["8107EC40", "8107EC42"]
-		},
-		"hair": {
-			"shadow": ["8107EC98", "8107EC9A"],
-			"main": ["8107ECA0", "8107ECA2"]
-		},
-		"skin": {
-			"shadow": ["8107EC80", "8107EC82"],
-			"main": ["8107EC88", "8107EC8A"]
-		},
-		"gloves": {
-			"shadow": ["8107EC50", "8107EC52"],
-			"main": ["8107EC58", "8107EC5A"]
-		},
-		"overalls": {
-			"shadow": ["8107EC20", "8107EC22"],
-			"main": ["8107EC28", "8107EC2A"]
-		},
-		"shoes": {
-			"shadow": ["8107EC68", "8107EC6A"],
-			"main": ["8107EC70", "8107EC72"]
-		}
-	},
-	"ntsc-j": {
-		"hat_shirt": {
-			"shadow": ["8107BDD8", "8107BDDA"],
-			"main": ["8107BDE0", "8107BDE2"]
-		},
-		"hair": {
-			"shadow": ["8107BE38", "8107BE3A"],
-			"main": ["8107BE40", "8107BE42"]
-		},
-		"skin": {
-			"shadow": ["8107BE20", "8107BE22"],
-			"main": ["8107BE28", "8107BE2A"]
-		},
-		"gloves": {
-			"shadow": ["8107BDF0", "8107BDF2"],
-			"main": ["8107BDF8", "8107BDFA"]
-		},
-		"overalls": {
-			"shadow": ["8107BDC0", "8107BDC2"],
-			"main": ["8107BDC8", "8107BDCA"]
-		},
-		"shoes": {
-			"shadow": ["8107BE08", "8107BE0A"],
-			"main": ["8107BE10", "8107BE12"]
-		}
-	},
-	"pal": {
-		"hat_shirt": {
-			"shadow": ["810742F8", "810742FA"],
-			"main": ["81074300", "81074302"]
-		},
-		"hair": {
-			"shadow": ["81074358", "8107435A"],
-			"main": ["81074360", "81074362"]
-		},
-		"skin": {
-			"shadow": ["81074340", "81074342"],
-			"main": ["81074348", "8107434A"]
-		},
-		"gloves": {
-			"shadow": ["81074310", "81074312"],
-			"main": ["81074318", "8107431A"]
-		},
-		"overalls": {
-			"shadow": ["810742E0", "810742E2"],
-			"main": ["810742E8", "810742EA"]
-		},
-		"shoes": {
-			"shadow": ["81074328", "8107432A"],
-			"main": ["81074330", "81074332"]
-		}
-	},
-	"shindou": {
-		"hat_shirt": {
-			"shadow": ["8107BDD8", "8107BDDA"],
-			"main": ["8107BDE0", "8107BDE2"]
-		},
-		"hair": {
-			"shadow": ["8107BE38", "8107BE3A"],
-			"main": ["8107BE40", "8107BE42"]
-		},
-		"skin": {
-			"shadow": ["8107BE20", "8107BE22"],
-			"main": ["8107BE28", "8107BE2A"]
-		},
-		"gloves": {
-			"shadow": ["8107BDF0", "8107BDF2"],
-			"main": ["8107BDF8", "8107BDFA"]
-		},
-		"overalls": {
-			"shadow": ["8107BDC0", "8107BDC2"],
-			"main": ["8107BDC8", "8107BDCA"]
-		},
-		"shoes": {
-			"shadow": ["8107BE08", "8107BE0A"],
-			"main": ["8107BE10", "8107BE12"]
-		}
-	},
-	"ique": {
-		"hat_shirt": {
-			"shadow": ["810610A8", "810610AA"],
-			"main": ["810610B0", "810610B2"]
-		},
-		"hair": {
-			"shadow": ["81061108", "8106110A"],
-			"main": ["81061110", "81061112"]
-		},
-		"skin": {
-			"shadow": ["810610F0", "810610F2"],
-			"main": ["810610F8", "810610FA"]
-		},
-		"gloves": {
-			"shadow": ["810610C0", "810610C2"],
-			"main": ["810610C8", "810610CA"]
-		},
-		"overalls": {
-			"shadow": ["81061090", "81061092"],
-			"main": ["81061098", "8106109A"]
-		},
-		"shoes": {
-			"shadow": ["810610D8", "810610DA"],
-			"main": ["810610E0", "810610E2"]
-		}
-	}
 }
